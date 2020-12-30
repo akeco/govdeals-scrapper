8598a73402dc3206701d9a43c90425b624ef5a05
Amer Keco <Amer.Keco.contractor@hilton.com>
2020-12-30 13:15:04 +0100

    Updated test

--- test.js
+++ test.js
@@ -1,17 +1,11 @@
-var test = require('tape');
-
-test('timing test', function (t) {
-    t.plan(2);
-
-    t.equal(typeof Date.now, 'function');
-    var start = Date.now();
-
-    setTimeout(function () {
-        t.equal(Date.now() - start, 100);
-    }, 100);
+const test = require("tape");
+test("test integer addition", (t) => {
+  t.plan(1);
+  t.equal(1 + 1, 2);
 });
 
-test('test using promises', async function (t) {
-    const result = await someAsyncThing();
-    t.ok(result);
+test("test string addition", (t) => {
+    t.plan(1);
+  // Expected to fail
+  t.equal("1" + "1", 2);
 });
